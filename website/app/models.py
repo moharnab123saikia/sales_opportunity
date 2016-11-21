f13d8eafd889b22b09e1401fc4bf9ed757c71bbc
Moharnab Saikia <moharnab123saikia@gmail.com>
2016-11-20 19:14:34 -0800

    added sentiments view template

--- website/app/models.py
+++ website/app/models.py
@@ -1,6 +1,6 @@
 from flask.ext.appbuilder import Model
 from flask.ext.appbuilder.models.mixins import AuditMixin, FileColumn, ImageColumn
-from sqlalchemy import Column, Integer, String, ForeignKey 
+from sqlalchemy import Column, Integer, String, ForeignKey, Float
 from sqlalchemy.orm import relationship
 """
 
@@ -10,4 +10,25 @@ AuditMixin will add automatic timestamp of created and modified by who
 
 
 """
-        
+
+class Sentiment(Model):
+    id = Column(Integer, primary_key=True)
+    phone = Column(String(50), unique = True, nullable=False)
+    sound = Column(Float)
+    fingerprint = Column(Float)
+    color = Column(Float)
+    headphone = Column(Float)
+    design = Column(Float)
+    size = Column(Float)
+    network = Column(Float)
+    battery = Column(Float)
+    camera = Column(Float)
+    safety= Column(Float)
+    memory= Column(Float)
+    intelligence= Column(Float)
+    performance= Column(Float)
+    assistant= Column(Float)
+
+
+    def __repr__(self):
+        return self.phone
--- website/app/views.py
+++ website/app/views.py
@@ -1,7 +1,9 @@
 from flask import render_template
 from flask.ext.appbuilder.models.sqla.interface import SQLAInterface
-from flask.ext.appbuilder import ModelView
+from flask.ext.appbuilder.charts.views import DirectByChartView
+from flask_appbuilder.views import ModelView
 from app import appbuilder, db
+from models import Sentiment
 
 """
     Create your Views::
@@ -24,6 +26,23 @@ from app import appbuilder, db
 def page_not_found(e):
     return render_template('404.html', base_template=appbuilder.base_template, appbuilder=appbuilder), 404
 
+
+class SentimentModelView(ModelView):
+    datamodel = SQLAInterface(Sentiment)
+
+class SentimentModelChartView(DirectByChartView):
+    datamodel = SQLAInterface(Sentiment)
+    chart_title = "feature sentiments"
+    # base_filters = "iphone"
+    definitions = [
+    {
+        'label': 'battery sentiment',
+        'group': 'phone',
+        'series': ['camera']
+    }
+]
+
 db.create_all()
 
-
+appbuilder.add_view(SentimentModelView, "List Sentiments", icon="fa-folder-open-o", category="Statistics")
+appbuilder.add_view(SentimentModelChartView, "List Sentiment Chart", icon="fa-folder-open-o", category="Statistics")
